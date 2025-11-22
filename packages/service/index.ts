7ac129e0337b93dcab7780dbf82fa6325bb2bcb1
Spandan Barve <spandan567@gmail.com>
2025-11-22 13:04:31 -0300

    sync

--- packages/service/index.ts
+++ packages/service/index.ts
@@ -1,7 +1,21 @@
+import { appd } from "./lib/appd";
+import { Router } from "./lib/Router";
+
+const router = new Router();
+
+router.get('/app-id', (ctx) => {
+    return ctx.ok({ appId: appd.getAppId() });
+});
+
+router.get('/agents/:id/pk', (ctx) => {
+
+    return ctx.ok({ status: 'ok' });
+});
+
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        return new Response("Hello from Bun server!");
+        return router.handle(req);
     },
 });
 
