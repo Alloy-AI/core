8467cd3c5dad6905e7ee81e2cb297041ae019b3a
Spandan Barve <spandan567@gmail.com>
2025-11-23 01:29:48 -0300

    synapse helpers

---
+++ packages/service/lib/synapse.ts
@@ -0,0 +1,30 @@
+import { RPC_URLS, Synapse } from "@filoz/synapse-sdk";
+import type { Address } from "viem";
+import { env } from "../env";
+import { tryCatch } from "./tryCatch";
+import { privateKeyToAddress } from "viem/accounts";
+
+// because filbeam links are very useful
+const WITH_CDN = true;
+
+export const serverAddressSynapse = privateKeyToAddress(env.EVM_PRIVATE_KEY_SYNAPSE)
+
+export async function getOrCreateDataset() {
+     const synapse = await Synapse.create({
+        privateKey: env.EVM_PRIVATE_KEY_SYNAPSE,
+        rpcURL: RPC_URLS.calibration.websocket,
+        withCDN: WITH_CDN,
+    });
+
+	const ctx = await tryCatch(
+		synapse.storage.createContext({
+			metadata: { },
+		}),
+	);
+
+	if (ctx.error) {
+		throw new Error("Fail to create synapse context for new user dataset");
+	}
+
+	return ctx.data;
+}
