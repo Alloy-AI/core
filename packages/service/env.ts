b5f8be49c7fbc9979801eee8aaf9fadb7bfbee71
Spandan Barve <spandan567@gmail.com>
2025-11-22 23:13:18 -0300

    env zod check idk who removed

--- packages/service/env.ts
+++ packages/service/env.ts
@@ -1,15 +1,13 @@
+import z from "zod";
+
 const envRaw = Bun.env;
 
-export const env = {
-    NODE_ENV: envRaw.NODE_ENV || "development",
-    GROQ_API_KEY: envRaw.GROQ_API_KEY,
-    PG_URI: envRaw.PG_URI,
-    PORT: parseInt(envRaw.PORT || "3000"),
-} as {
-    NODE_ENV: string;
-    GROQ_API_KEY: string;
-    PG_URI: string;
-    PORT: number;
-};
+export const env = z.object({
+    NODE_ENV: z.string().default("development"),
+    GROQ_API_KEY: z.string(),
+    PG_URI: z.string(),
+    PORT: z.number().min(1).default(3000),
+    EVM_PRIVATE_KEY_SYNAPSE: z.string(),
+}).parse(envRaw)
 
 export const isProd = env.NODE_ENV === "production";
