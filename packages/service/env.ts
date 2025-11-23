ca65780b60b02529574b2650c07101a5bf5f4ef3
Yash <contact@yashgoyal.dev>
2025-11-23 01:57:19 -0300

    fixed a bug

--- packages/service/env.ts
+++ packages/service/env.ts
@@ -6,7 +6,7 @@ export const env = z.object({
     NODE_ENV: z.string().default("development"),
     GROQ_API_KEY: z.string(),
     PG_URI: z.string(),
-    PORT: z.number().min(1).default(3000),
+    PORT: z.string().default("3000").transform((port) => parseInt(port)),
     EVM_PRIVATE_KEY_SYNAPSE: z.string().startsWith("0x").transform((key) => key as `0x${string}`),
     GEMINI_API_KEY: z.string(),
 }).parse(envRaw)
